9da8e87ef9b2b8b0365fd4f778f688b4d4eb804d
Matthew Baty <mattbaty@outlook.com>
2023-04-16 15:28:55 -0400

    updated scehma.sql file

--- db/schema.sql
+++ db/schema.sql
@@ -21,8 +21,10 @@ CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT,
-    manager_id INT,
+    role_id INT NOT NULL,
     FOREIGN KEY (role_id)
     REFERENCES role(id)
+    manager_id INT,
+    FOREIGN KEY (employee_id)
+    REFERENCES employee(id)
 );
